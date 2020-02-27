a10e8b17bad8fbd68f10f3ab56ecbf21015a1992
Mark Lewis <mlewis@trinity.edu>
2020-02-26 22:02:35 -0600

    Added additional test for task list version 1 to more completely test the application.

--- server/test/TaskList1Spec.scala
+++ server/test/TaskList1Spec.scala
@@ -5,7 +5,7 @@ import org.scalatestplus.play.HtmlUnitFactory
 
 class TaskList1Spec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory {
   "Task list 1" must {
-    "login and access functions" in {
+    "login and see task list" in {
       go to s"http://localhost:$port/login1"
       pageTitle mustBe "Login"
       find(cssSelector("h2")).isEmpty mustBe false
@@ -19,7 +19,62 @@ class TaskList1Spec extends PlaySpec with GuiceOneServerPerSuite with OneBrowser
         pageTitle mustBe "Task List"
         find(cssSelector("h2")).isEmpty mustBe false
         find(cssSelector("h2")).foreach(e => e.text mustBe "Task List")
-        findAll(cssSelector("li")).toList.map(_.text) mustBe List("Make videos", "eat","sleep","code")
+        findAll(cssSelector("li")).toList.map(_.text) mustBe List("Make videos", "eat","sleep", "code")
+      }
+    }
+
+    "add task in" in {
+      pageTitle mustBe "Task List"
+      click on "newTask"
+      textField("newTask").value = "test"
+      submit()
+      eventually {
+        pageTitle mustBe "Task List"
+        findAll(cssSelector("li")).toList.map(_.text) mustBe List("test", "Make videos", "eat", "sleep","code")
+      }
+    }
+
+    "delete task" in {
+      pageTitle mustBe "Task List"
+      click on "delete-3"
+      eventually {
+        pageTitle mustBe "Task List"
+        findAll(cssSelector("li")).toList.map(_.text) mustBe List("test", "Make videos", "eat", "code")
+      }
+    }
+
+    "logout" in {
+      pageTitle mustBe "Task List"
+      click on "logout"
+      eventually {
+        pageTitle mustBe "Login"
+      }
+    }
+
+    "create user and add two tasks" in {
+      pageTitle mustBe "Login"
+      click on "username-create"
+      textField("username-create").value = "Lewis"
+      click on "password-create"
+      pwdField(id("password-create")).value = "foo"
+      submit()
+      eventually {
+        pageTitle mustBe "Task List"
+        findAll(cssSelector("li")).toList.map(_.text) mustBe Nil
+        click on "newTask"
+        textField("newTask").value = "test1"
+        submit()
+        eventually {
+          pageTitle mustBe "Task List"
+          findAll(cssSelector("li")).toList.map(_.text) mustBe List("test1")
+          click on "newTask"
+          textField("newTask").value = "test2"
+          submit()
+          eventually {
+            pageTitle mustBe "Task List"
+            findAll(cssSelector("li")).toList.map(_.text) mustBe List("test2", "test1")
+          }
+        }
       }
     }
   }  
