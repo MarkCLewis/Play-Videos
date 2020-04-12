de2c5520acdd9c658a2432b5bd5ae05489e52da1
Mark Lewis <mlewis@trinity.edu>
2020-04-12 08:49:37 -0500

    Done with the general introduction to Scala.js.

--- scalajs-only/src/main/scala/example/Hello.scala
+++ scalajs-only/src/main/scala/example/Hello.scala
@@ -9,6 +9,29 @@ import org.scalajs.dom.html
 object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello World.")
-    for (i <- 1 to 10) println(i) 
+    for (i <- 1 to 10) println(i)
+
+    document.getElementById("title").innerHTML = "Scala.js"
+    document.getElementById("content").innerHTML = "This is a tutorial on using Scala.js."
+
+    appendParagraph(document.getElementById("content"), "This is a new paragraph.")
+    drawToCanvas(document.getElementById("canvas").asInstanceOf[html.Canvas])
+  }
+
+  def appendParagraph(target: dom.Node, text: String): Unit = {
+    val p = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    p.appendChild(textNode)
+    target.appendChild(p)
+  }
+
+  @JSExportTopLevel("doClickStuff")
+  def doClickStuff(): Unit = {
+    println("Button clicked.")
+  }
+
+  def drawToCanvas(canvas: html.Canvas): Unit = {
+    val context = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+    context.fillRect(100, 100, 200, 150)
   }
 }
