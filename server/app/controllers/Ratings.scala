d14f6fd37b7c73a9e3bb3ee211ec9a87b5d969d9
Pumplard <pumplard@gmail.com>
2022-05-04 23:52:54 -0500

    added redirects on Student profile

--- server/app/controllers/Ratings.scala
+++ server/app/controllers/Ratings.scala
@@ -15,20 +15,22 @@ import models.RatingsInMemoryModel
 class Ratings @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   //Presents a test-page for the rating system
-  //currently takes a username as input for displaying the page
+  //currently takes a username/course name as input for displaying the page
   def ratings() = Action { implicit request =>
   val user = "Dr. Professor"
-  Ok(views.html.ratingTest(user)(RatingsInMemoryModel.getRating(user)))
+  Ok(views.html.ratings(user)(RatingsInMemoryModel.getRating(user)))
   }  
   
-  //Adds rating to current professor/course
+  //Adds rating to current professor/course on scale of 0.0 to 5.0
   //Doesn't work with empty string
   def rate = Action { implicit request =>
     val user = "Dr. Professor"
     val postVals = request.body.asFormUrlEncoded
     postVals.map { args =>
-      val rating = args("newRating").head
-      RatingsInMemoryModel.addRating(user, rating.toDouble);
+      val rating = (args("newRating").head).toDouble
+      if (rating > 0.0 && rating <= 5.0) {
+        RatingsInMemoryModel.addRating(user, rating);
+      }
       Redirect(routes.Ratings.ratings())
     }.getOrElse(Redirect(routes.Ratings.ratings()))
   }
